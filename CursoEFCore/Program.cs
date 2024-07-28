5258ced1bffb359c71c0c4c4610d83831612ccc3
ezequiel-lima <ezequiel.lima.br@gmail.com>
2024-07-27 22:41:32 -0300

    adicionado metodo de adicionar produto

--- CursoEFCore/Program.cs
+++ CursoEFCore/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CursoEFCore.Data;
+using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
 
+InserirDados();
 Console.WriteLine("Hello, World!");
+
+void InserirDados()
+{
+    var produto = new Produto
+    {
+        Descricao = "Produto teste",
+        CodigoBarras = "1234567891231",
+        Valor = 10m,
+        TipoProduto = TipoProduto.MercadoriaParaRevenda,
+        Ativo = true
+    };
+
+    using var db = new ApplicationContext();
+
+    db.Produtos.Add(produto);
+    //db.Set<Produto>().Add(produto);               // Opções para adiconar no entity framework core
+    //db.Entry(produto).State = EntityState.Added;
+    //db.Add(produto);
+
+    var registros = db.SaveChanges();
+    Console.WriteLine($"Registros afetados: {registros}");
+}
